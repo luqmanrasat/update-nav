44bbb530c1bc55cd766fb58ffe32c731530e5ae9
Luqman Rasat <luqmanrasat@goodpeople.my>
2019-10-01 18:03:10 +0800

    change to retrieve values from excel

--- config_example.js
+++ config_example.js
@@ -1,8 +1,3 @@
 module.exports = {
   projectPath: "/User/Satoshi/Documents/rhb-sg-ui",
-  abbreviations: {
-    rsif: "RHB Singapore Income Fund",
-    repf: "RHB Entrepreneur Fund",
-    rcaf: "RHB China-ASEAN Fund",
-  }
 }
--- index.js
+++ index.js
@@ -1,62 +1,50 @@
 const path = require('path');
 const fs = require('fs');
-const utils = require('./utils');
+const utils = require('./lib/utils');
 const { projectPath } = require('./config');
 
 (async () => {
   try {
+    const xlsxFilePath = path.resolve(__dirname, 'nav-datasheet.xlsx');
+    const jadeFilePath = path.join(projectPath, "views/enums/enums-asset-management-overview.jade");
+    
     const branch = 'update/nav-value';
     await utils.gitCheckoutPull(branch);
-    const filePath = path.join(projectPath, "views/enums/enums-asset-management-overview.jade");
-    let readFile = fs.readFileSync(filePath, { encoding: 'utf8'});
+    let readFile = fs.readFileSync(jadeFilePath, { encoding: 'utf8'});
   
     const patternFund = new RegExp(utils.pattern.fund, 'g');
     const fundLines = readFile.match(patternFund);
-    const fundsList = fundLines.map(line => line.replace(patternFund, "$1"));
     const patternDate = new RegExp(utils.pattern.date, 'g');
     const dateLine = readFile.match(patternDate).pop();
 
-    let newFunds;
-    let newDate;
-    let confirm = false;
-    while (!confirm) {
-      newFunds = [];
-      for (const fund of fundsList) {
-        const newValue = await utils.promptFund(fund);
-        newFunds.push(newValue);
-      }
-      newDate = await utils.promptDate();
-      confirm = await utils.promptConfirm();
-    }
-    
+    const { newNav, newDate } = await utils.getNavFromXlsx(xlsxFilePath);
+
     console.log('Updating nav values...')
-    let newFile = utils.updateFunds(fundLines, newFunds, readFile);
+    console.log("funds: "+ newNav)
+    console.log("date: "+ newDate)
+    let newFile = utils.updateFunds(fundLines, newNav, readFile);
     newFile = utils.updateDate(dateLine, newDate, newFile);
-    fs.writeFileSync(filePath, newFile);
+    fs.writeFileSync(jadeFilePath, newFile);
     console.log('Nav values updated');
 
-    await utils.gitAddCommit(filePath, 'Update nav value');
+    await utils.gitAddCommit(jadeFilePath, 'Update nav value');
     await utils.gitPush(branch)
 
     const mergeStaging = await utils.promptMerge('staging');
-    if (mergeStaging) {
-      await utils.gitCheckoutPull('staging');
-      await utils.gitMerge(branch);
-      await utils.gitPush('staging');
-    }
-    else {
+    if (!mergeStaging) {
       throw new Error('Nav updated without merging to staging & master branch')
     }
+    await utils.gitCheckoutPull('staging');
+    await utils.gitMerge(branch);
+    await utils.gitPush('staging');
     
     const mergeMaster = await utils.promptMerge('master');
-    if (mergeMaster) {
-      await utils.gitCheckoutPull('master');
-      await utils.gitMerge(branch);
-      await utils.gitPush('master')
-    }
-    else {
+    if (!mergeMaster) {
       throw new Error('Nav updated without merging to master branch')
     }
+    await utils.gitCheckoutPull('master');
+    await utils.gitMerge(branch);
+    await utils.gitPush('master');
 
     console.log('Done without error');
   } catch (error) {
